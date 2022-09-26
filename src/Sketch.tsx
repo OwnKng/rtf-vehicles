3df25777c1248571c53473447ce4aa621ddba3aa
OwnKng <owen.king@outlook.com>
2022-09-26 23:45:11 +0100

    UseVehciles

--- src/Sketch.tsx
+++ src/Sketch.tsx
@@ -1,71 +1,44 @@
 import { useFrame } from "@react-three/fiber"
-import { useRef, useState } from "react"
 import * as THREE from "three"
-import { useVehicle } from "./hooks/useVehicle"
+import { useVehicles } from "./hooks/useVehicles"
 import { random } from "./utils"
+import { applyForce, wander } from "./utils/vehicle"
 
-const vehicleData = {
+const vehicleData = Array.from({ length: 40 }, () => ({
   position: new THREE.Vector3(random(0, 10), random(0, 10), random(0, 10)),
   acceleration: new THREE.Vector3(0, 0, 0),
   velocity: new THREE.Vector3(0, 0, 0),
   maxSpeed: 0.2,
   maxForce: 0.01,
-}
-
-type birdProps = {
-  home: THREE.Vector3
-  predator: THREE.Vector3 | null
-}
-
-const Bird = ({ home, predator }: birdProps) => {
-  const [vehicle, api] = useVehicle(vehicleData)
-
-  useFrame(() => {
-    const distanceToPredator = api.distanceTo(predator)
-
-    const force =
-      distanceToPredator > 10 ? api.arrive(home) : api.flee(predator)
-    api.applyForce(force)
-  })
-
-  return (
-    <mesh ref={vehicle}>
-      <cylinderBufferGeometry args={[0, 0.5, 2, 3]} />
-      <meshPhongMaterial flatShading shininess={1} color='#5ADBFF' />
-    </mesh>
-  )
-}
-
-const r = 10
-const homePosition = new THREE.Vector3(0, 1, 0)
+  latitude: 0,
+  longitude: 0,
+  world: {
+    width: 20,
+    height: 20,
+    depth: 20,
+  },
+}))
 
 const Sketch = () => {
-  const ref = useRef<THREE.Mesh>(null!)
-  const [predPosition, setPredPosition] = useState<THREE.Vector3>(
-    new THREE.Vector3(0, 0, 0)
-  )
+  const [vehicles, { at }] = useVehicles(vehicleData)
 
   useFrame(() => {
-    const { x, y, z } = predPosition
-    ref.current.position.set(x, y, z)
+    const force = wander(20, at(10))
+    applyForce(force, at(10))
   })
 
   return (
     <>
-      <Bird home={homePosition} predator={predPosition} />
-      <mesh position={[homePosition.x, homePosition.y - 1, homePosition.z]}>
-        <boxBufferGeometry args={[2, 0.1, 2]} />
-        <meshPhongMaterial flatShading shininess={1} color='#F2E863' />
-      </mesh>
-      <mesh
-        rotation={[-Math.PI * 0.5, 0, 0]}
-        onPointerMove={(e) => setPredPosition(e.point)}
+      <instancedMesh
+        //@ts-ignore
+        ref={vehicles}
+        args={[undefined, undefined, 40]}
       >
+        <cylinderBufferGeometry args={[0, 0.5, 2, 3]} />
+        <meshPhongMaterial flatShading shininess={1} color='#5ADBFF' />
+      </instancedMesh>
+      <mesh rotation={[-Math.PI * 0.5, 0, 0]}>
         <planeBufferGeometry args={[40, 40, 5, 5]} />
-        <meshBasicMaterial wireframe />
-      </mesh>
-      <mesh ref={ref} rotation={[-Math.PI * 0.5, 0, 0]}>
-        <circleBufferGeometry args={[r, 16]} />
         <meshBasicMaterial wireframe />
       </mesh>
     </>
